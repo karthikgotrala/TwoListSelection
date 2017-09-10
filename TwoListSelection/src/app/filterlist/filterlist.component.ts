cffd863310078daf3f3ca6e085293c60fe21fc28
karthikgotrala <Karthik.Gotrala@hhsc.state.tx.us>
2017-09-10 00:38:06 -0500

    commit

--- TwoListSelection/src/app/filterlist/filterlist.component.ts
+++ TwoListSelection/src/app/filterlist/filterlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-filterlist',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filterlist.component.css']
 })
 export class FilterlistComponent implements OnInit {
-
+ @Input() listContent:{label:string,listItems:{code:string;label:string;}[]};
+    
   constructor() { }
 
   ngOnInit() {
--- TwoListSelection/src/app/twolistselction/twolistselction.component.ts
+++ TwoListSelection/src/app/twolistselction/twolistselction.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./twolistselction.component.css']
 })
 export class TwolistselctionComponent implements OnInit {
-
+ leftContent={
+     label:"Available",
+     listItems:[
+         {"code":"1","label":"Austin"},
+         {"code":"2","label":"Round Rock"},
+         {"code":"3","label":"Cedar Park"},
+         {"code":"4","label":"Leander"},
+         {"code":"5","label":"Pflugerville"},
+         {"code":"6","label":"Georgetown"},
+         {"code":"7","label":"Buda"},
+         {"code":"8","label":"Kyle"},
+         {"code":"9","label":"San Marcos"},
+         {"code":"10","label":"Hutto"},
+         {"code":"11","label":"Drpping Springs"},
+         {"code":"12","label":"New Braunfels"},
+         {"code":"13","label":"Bastrop"},
+         
+         
+         
+         
+     ]
+     
+ }
   constructor() { }
 
   ngOnInit() {
   }
+    
+    
 
 }
